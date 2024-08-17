aff5d3edc5d036604e2358d3059cc2f1e5093fdc
WilliamKSilva <wilz.kelvin12@gmail.com>
2024-08-17 12:30:23 -0300

    feat: add Problem 7 solution

---
+++ p7.ml
@@ -0,0 +1,23 @@
+(* Flatten a nested list structure. *)
+
+type 'a node = One of 'a | Many of 'a node list
+
+let flatten (l : 'a node list) =
+  let rec loop l flatten =
+    match l with
+    | [] -> flatten
+    | hd :: tl -> (
+        match hd with
+        | One one ->
+            let u = List.append flatten [ one ] in
+            loop tl u
+        | Many many ->
+            let f = loop many flatten in
+            if List.length tl >= 1 then loop tl f else f)
+  in
+
+  loop l []
+
+(* res = ["a"; "b"; "c"; "d"; "e"] *)
+let res =
+  flatten [ One "a"; Many [ One "b"; Many [ One "c"; One "d" ]; One "e" ] ]
