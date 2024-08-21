605b7e32dea65e5d1d1f48303453e50877b77518
WilliamKSilva <wilz.kelvin12@gmail.com>
2024-08-21 11:25:02 -0300

    feat: add Problem 13 solution

---
+++ p13.ml
@@ -0,0 +1,43 @@
+type 'a rle =
+  | One of 'a
+  | Many of int * 'a
+
+let encode l =
+  let rec aux l encoded =
+    match l with
+    | [] -> encoded
+    | hd :: tl ->
+      match encoded with
+      | [] ->
+        let encoded = List.append encoded [One hd] in
+        aux tl encoded
+      | last :: encoded_tl ->
+        match last with
+        | One a ->
+          if a = hd then
+            let many = Many (2, a) in
+            let encoded = many :: encoded_tl in
+            aux tl encoded
+          else
+            let one = One hd in
+            (* We add to the unmodified encoded List *)
+            let encoded = one :: encoded in
+            aux tl encoded
+        | Many (fst, sec) ->
+          if sec = hd then
+            let many = Many (fst + 1, sec) in
+            let encoded = many :: encoded_tl in
+            aux tl encoded
+          else
+            let one = One hd in
+            (* We add to the unmodified encoded List *)
+            let encoded = one :: encoded in
+            aux tl encoded
+  in
+
+  let encoded = aux l [] in
+  List.rev encoded
+
+(* res = [Many (4, "a"); One "b"; Many (2, "c"); Many (2, "a"); One "d";
+ Many (4, "e")]  *)
+let res = encode ["a";"a";"a";"a";"b";"c";"c";"a";"a";"d";"e";"e";"e";"e"]
