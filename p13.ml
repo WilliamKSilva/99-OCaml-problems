09d4c8458dd3c1a3eda88acccd51065bd23f0c38
WilliamKSilva <wilz.kelvin12@gmail.com>
2024-08-21 11:37:06 -0300

    feat: add missing definition of Problem 13

--- p13.ml
+++ p13.ml
@@ -1,3 +1,10 @@
+(*
+  Implement the so-called run-length encoding data compression method directly. I.e. don't explicitly
+  create the sublists containing the duplicates, as in problem "Pack consecutive duplicates of list
+  elements into sublists", but only count them. As in problem "Modified run-length encoding",
+  simplify the result list by replacing the singleton lists (1 X) by X.
+*)
+
 type 'a rle =
   | One of 'a
   | Many of int * 'a
